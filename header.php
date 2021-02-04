2e09342ed3a43062854d2a5d02b045e0b7e86f67
Erik Jung <erik.jung@5pectre.com>
2021-02-04 03:32:24 +0100

    hinzufügen des Portfolios

--- header.php
+++ header.php
@@ -34,7 +34,19 @@
 			<div class="wrapper">
 				<div class="logo">
 						<img src="<?php echo get_template_directory_uri(); ?>/img/logo/logo_sh_light.svg" alt="">
-						<p class="menu-link" id="toggleNav">Menu</p>
+						<p class="menu-link">
+							<svg class="ham hamRotate" viewBox="0 0 100 100" width="80" onclick="this.classList.toggle('active')">
+							  <path
+							        class="line top"
+							        d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20" />
+							  <path
+							        class="line middle"
+							        d="m 30,50 h 40" />
+							  <path
+							        class="line bottom"
+							        d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20" />
+							</svg>
+						</p>
 				</div>
 				<div class="navigation">
 					<?php wp_nav_menu(array( 'theme_location' => 'main' )); ?>
